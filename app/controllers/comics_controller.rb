a89de506ac76b064511b3c68cf05959e9c44d648
runanakaue <138458451+runanakaue@users.noreply.github.com>
2023-08-20 10:19:16 +0000

    third commit

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,5 +1,6 @@
 class ComicsController < ApplicationController
   before_action :require_permission, only:[:edit, :update, :destroy]
+  before_action :authorize_user, only: [:edit, :update]
 
   def new
     @comic = Comic.new
@@ -18,19 +19,60 @@ class ComicsController < ApplicationController
   def edit
     @comic = Comic.find(params[:id])
     @comic.save
+    if @comic.user != current_user
+      redirect_to comics_path
+    end
   end
+
 
   def create
     @comic = current_user.comics(comic_params)
+    if @comic.save
+      flash[:notice] = 'Comic was successfully created.'
+      redirect_to comic_path(@comic.id)
+    else
+      @user = current_user
+      @comics = Comic.all
+      render :index
+    end
   end
 
   def update
     @comic = Comic.find(params[:id])
+    if @comic.update(comic_params)
+      flash[:notice] = 'Comic was successfully updated.'
+      redirect_to comic_path(@comic.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
     @comic = Comic.find(params[:id])
+    if @comic.destroy
+      flash[:notice] = 'Comic was successfully destroyed.'
+      redirect_to comics_path
+    end
   end
 
   private
+
+  def comic_params
+    params.require(:comic).permit(:title, :body)
+  end
+
+  def require_permission
+    @comic = Comic.find(params[:id])
+    if @comic.user != current_user
+      redirect_to comics_path, alert: "他人の投稿は編集・削除できません。"
+    end
+  end
+
+  def authorize_user
+    @comic = Comic.find(params[:id])
+    unless current_user == @comic.user
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,26 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
     @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'You have updated user successfully.'
+      redirect_to user_path(@user)
+    else
+      flash[:error] = 'Failed to update user.'
+      render :edit
+    end
   end
 
   private
 
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 
 end
